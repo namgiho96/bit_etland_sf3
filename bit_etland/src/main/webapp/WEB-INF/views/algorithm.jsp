24cc466ae29b4075786bb7885e84b9b32cd02d52
hb1005 <hb1005@hb1005>
2019-03-20 17:38:03 +0900

    2019-03-20 17:37

--- bit_etland/src/main/webapp/WEB-INF/views/algorithm.jsp
+++ bit_etland/src/main/webapp/WEB-INF/views/algorithm.jsp
@@ -67,24 +67,23 @@
       </div>
     </div>
 
-    <div class="col-sm-9">
+    <div id="right_content" class="col-sm-9" >
       <h4><small>RECENT POSTS</small></h4>
       <hr>
-      <h2 id="que_1">I Love Food</h2>
+      <h2 id="question">I Love Food</h2>
       <h5><span class="glyphicon glyphicon-time"></span> Post by Jane Dane, Sep 27, 2015.</h5>
       <h5><span id="answer_btn_1" class="label label-danger" >Food</span> <span id="answer_btn_2" class="label label-primary">Ipsum</span></h5><br>
       <p  id="result_1">Food is my passion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <br><br>
       
-      <h4><small>RECENT POSTS</small></h4>
-      <hr>
+      <h4 id="rm_start" id="rm"><small id="small">RECENT POSTS</small></h4>
       <h2 id="que_2">Officially Blogging</h2>
-      <h5><span class="glyphicon glyphicon-time"></span> Post by John Doe, Sep 24, 2015.</h5>
-      <h5><span class="label label-success">Lorem</span></h5><br>
+      <h5 id="time"><span class="glyphicon glyphicon-time"></span> Post by John Doe, Sep 24, 2015.</h5>
+      <h5 id="success"><span class="label label-success">Lorem</span></h5><br>
       <p id="result_2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <hr>
+      <hr id="rm_end">
 
-      <h4>Leave a Comment:</h4>
+      <h4 id="leave_a_comment">Leave a Comment:</h4>
       <form role="form">
         <div class="form-group">
           <textarea class="form-control" rows="3" required></textarea>
--- bit_etland/src/main/webapp/resources/js/algorithm.js
+++ bit_etland/src/main/webapp/resources/js/algorithm.js
@@ -8,54 +8,32 @@ algo =(()=>{
 		};
 		let setContentView=()=>{
 			$.getScript($.js()+'/compo.js',()=>{
-				/*$('body').html($$.div({id: 'test',clazz:'clazz'}))*/
-				$('#nav').children().eq(0)
-						.html($$.a({id:'seq', url:'#'}).text('수열'))
-				$('#nav').children().eq(1)
-						.html($$.a({id:'math',url:'#'}).text('수학'))
-				$('#nav').children().eq(2)
-						.html($$.a({id:'arr',url:'#'}).text('배열'))
-				$('#nav').children().eq(3)
-						.html($$.a({id:'coll',url:'#'}).text('자료구조'))
-				$('#nav').append($$.li({id:''}))
-						.children().eq(4).html($$.a({id:'app',}).text('응용'));
-				$('#que_1').text('수열 1: 등차수열의 합계');
-				input_1();
-				$('#answer_btn_1').text('결과').addClass('cursor')
-					.click(()=>{
-						$.ajax({
-							url : $.ctx()+'/algo/seq/1',
-							type : 'post',
-							data : JSON.stringify({
-								start: $('#start').val(),
-								end : $('#end').val(),
-								diff : $('#diff').val()}),
-							datatype: 'json',
-							contentType : "application/json",
-							success : d=>{
-								alert('넘어온 문제번호 :'+d.result);
-									$('#result_1').html($$.h({id:'h_res',size:'2'})
-										.text('결과값  : '+d.result));
-							},
-							error : e=>{
-								alert('AJAX 실패');
-							}
-						});
-					});	
-				$('#answer_btn_2').text('리셋').addClass('cursor')
-					.click(()=>{
-						input_1();
-					})
+				drop();
+				nav();
+				question('등차수열');
+				
+				$('#right_content').prepend($$.div({id:"right_start"}));
+				$('#leave_a_comment').before('<div id="right_end" />');
+				$('#right_start').nextUntil('#right_end')
+					.wrapAll('<div id="new_div"></div>');
+				var str = $('#new_div').html();
+				$('#new_div').remove();
+				$('#right_end').remove();
+				let arr = [
+					{id:'a', val:'등차수열'},
+					{id:'b', val:'등비수열'},
+					{id:'c', val:'팩토리얼수열'},
+					{id:'d', val:'피보나치수열'}
+				];
+				$.each(arr,(i,j)=>{
+					$('#right_start').append(str);
+					$('#question').attr('id','question_'+j.id);
+					$('#question_'+j.id).text(j.val);
 					
-					$('#que_2').text('수열 2: 등비수열의 합계');
-					input_2();
-				
-					
-				
-				
-				
+				});
 			})
 		};
+		
 		let input_1 =()=>{
 			$('#result_1').html($$.input({type:'text',id:'start'}));
 			$('#result_1').prepend('시작값 : ');
@@ -64,14 +42,53 @@ algo =(()=>{
 			$('#result_1').append('<br/> <br/> 공차 : ');
 			$('#result_1').append($$.input({type:'text',id:'diff'}));
 		}
-		let input_2 =()=>{
-			$('#result_2').html($$.input({type:'text',id:'start'}));
-			$('#result_2').prepend('시작값 : ');
-			$('#result_2').append('<br/> <br/> 한계값 : ');
-			$('#result_2').append($$.input({type:'text',id:'end'}));
-			$('#result_2').append('<br/> <br/> 공비 : ');
-			$('#result_2').append($$.input({type:'text',id:'diff'}));
-			
+		let question =(x)=>{
+			$('#question').text(x);
+			input_1();
+			$('#answer_btn_1').text('결과').addClass('cursor')
+				.click(()=>{
+					$.ajax({
+							url : $.ctx()+'/algo/seq/1',
+							type : 'post',
+							data : JSON.stringify({
+							start: $('#start').val(),
+							end : $('#end').val(),
+							diff : $('#diff').val()}),
+							datatype: 'json',
+							contentType : "application/json",
+							success : d=>{
+							alert('넘어온 문제번호 :'+d.result);
+								$('#result_1').html($$.h({id:'h_res',size:'2'})
+									.text('결과값  : '+d.result));
+							},
+							error : e=>{
+							alert('AJAX 실패');
+							}
+					});
+				});	
+			$('#answer_btn_2').text('리셋').addClass('cursor')
+				.click(()=>{
+					input_1();
+				})
+				
+		}
+		let nav =()=>{
+			$('#nav').children().eq(0)
+			.html($$.a({id:'seq', url:'#'}).text('수열'))
+			$('#nav').children().eq(1)
+					.html($$.a({id:'math',url:'#'}).text('수학'))
+			$('#nav').children().eq(2)
+					.html($$.a({id:'arr',url:'#'}).text('배열'))
+			$('#nav').children().eq(3)
+					.html($$.a({id:'coll',url:'#'}).text('자료구조'))
+			$('#nav').append($$.li({id:''}))
+					.children().eq(4).html($$.a({id:'app',}).text('응용'));
+		}
+		let drop =()=>{
+			$('#rm_start').before('<div id="del_start"/>')
+			$('#rm_end').after('<div id="del_end">')
+			$('#del_start').nextUntil('#del_end')
+				.wrap().remove();
 		}
 		return{init:init,
 			onCreate:onCreate};
